1ee3a5f886861e18019b69414af6849873f2c185
carlalennon <carlalennon@gmail.com>
2024-11-27 15:23:38 +0000

    Add onclick event to red button

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ var stage = new Konva.Stage({
 
 // then create layer
 var layer = new Konva.Layer();
-
 var gridLayer = new Konva.Layer();
+var buttonLayer = new Konva.Layer();
 
+// grid 
 var gridSize = 10; 
 
 for (var y = 0; y < stage.height(); y += gridSize) {
   var gridX = new Konva.Line({
     points: [0, y, stage.width(), y],
-    stroke: 'blue',
+    stroke: 'lightblue',
     strokeWidth: 1,
   });
   gridLayer.add(gridX);
@@ -24,29 +25,46 @@ for (var y = 0; y < stage.height(); y += gridSize) {
 for (var x = 0; x < stage.width(); x += gridSize) {
   var gridY = new Konva.Line({
     points: [x, 0, x, stage.height()],
-    stroke: 'blue',
+    stroke: 'lightblue',
     strokeWidth: 1,
   });
   gridLayer.add(gridY);
 }
 
 // create our shape
-var circle = new Konva.Circle({
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  radius: 70,
+var redButton = new Konva.Rect({
+  x: 10,
+  y: 440,
+  width: 100,
+  height: 50,
   fill: 'red',
   stroke: 'black',
   strokeWidth: 4
 });
 
-// add the shape to the layer
-layer.add(circle);
+var redButtonText = new Konva.Text({
+  x: 30,
+  y: 450,
+  text: 'Round',
+  fontSize: 20,
+  fontFamily: 'Calibri',
+  fill: 'white'
+});
 
+buttonLayer.add(redButton, redButtonText);
+
+// event 
+redButton.on('click', function() {
+  console.log('clicked');
+});
+
+// add the shape to the layer
 // add the layers to the stage
 stage.add(layer);
 stage.add(gridLayer);
+stage.add(buttonLayer);
 
 // draw the image
 layer.draw();
 gridLayer.draw();
+buttonLayer.draw();
